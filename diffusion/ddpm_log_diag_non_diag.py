4b4004720293cbc6ee48b30bcd25a793bdbc91ae
hlpmalshan <hlpmalshan@gmail.com>
2025-01-08 15:44:21 +0530

    Update ddpm_log_diag_non_diag.py

--- diffusion/ddpm_log_diag_non_diag.py
+++ diffusion/ddpm_log_diag_non_diag.py
@@ -239,8 +239,7 @@ class DDPM(pl.LightningModule):
         # squared_norm_preds = torch.mean(torch.sum(eps_pred**2, dim=2)) / dim_
         
         # compute the covariance matrix
-        print("shape", eps_pred.shape)
-        covariance_matrix = torch.matmul(eps_pred.T, eps_pred)
+        covariance_matrix = eps_pred.T@eps_pred/eps_pred.shape[0]
         
         diag_elements_mean = covariance_matrix.diagonal().mean()
         non_diag_elements_mean = torch.mean(torch.tril(covariance_matrix, diagonal=-1))

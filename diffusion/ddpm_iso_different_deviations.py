ff5ca320e3c5efa388895363e882db0facb8f5b5
hlpmalshan <hlpmalshan@gmail.com>
2025-02-24 04:42:28 +0530

    Updated diffusion\ddpm_iso_different_deviations.py

--- diffusion/ddpm_iso_different_deviations.py
+++ diffusion/ddpm_iso_different_deviations.py
@@ -111,7 +111,7 @@ class DDPM(pl.LightningModule):
     def diffuse_step(self, x, tidx):
         '''Simulate single forward process step.'''
         beta = self.betas[tidx]
-        eps = torch.randn_like(x) * torch.sqrt(self.target_variance)
+        eps = torch.randn_like(x) * torch.sqrt(torch.tensor(self.target_variance))
         x_noisy = (1 - beta).sqrt() * x + beta.sqrt() * eps
         return x_noisy
 
@@ -134,7 +134,7 @@ class DDPM(pl.LightningModule):
     def diffuse(self, x0, tids, return_eps=False):
         '''Simulate multiple forward steps at once.'''
         alpha_bar = self.alphas_bar[tids]
-        eps = torch.randn_like(x0)* torch.sqrt(self.target_variance)
+        eps = torch.randn_like(x0)* torch.sqrt(torch.tensor(self.target_variance))
 
         missing_shape = [1] * (eps.ndim - alpha_bar.ndim)
         alpha_bar = alpha_bar.view(*alpha_bar.shape, *missing_shape)
